3153212ce0da3773b894b51ef8018ed12ad25320
CrisUwU117 <86508220+CrisUwU117@users.noreply.github.com>
2023-07-04 00:13:40 -0600

    obtener imagenes

--- backend/controllers/images.controlles.js
+++ backend/controllers/images.controlles.js
@@ -19,3 +19,22 @@ import {db} from '../db.js'
     }
   });
 }
+export const obtenerImagenes  = (req, res) => {
+  const { id_estudiante, id_docente } = req.query;
+
+  const query = `SELECT id, nombre_archivo FROM imagen WHERE id_estudiante = ? OR id_docente = ?`;
+  const values = [id_estudiante, id_docente];
+
+  db.query(query, values, (error, results) => {
+    if (error) {
+      console.error('Error al obtener las imágenes de la base de datos:', error);
+      res.status(500).json({ error: 'Error al obtener las imágenes de la base de datos' });
+    } else {
+      const imagenes = results.map((imagen) => ({
+        id: imagen.id,
+        url: `\\${imagen.nombre_archivo}`
+      }));
+      res.status(200).json(imagenes);
+    }
+  });
+};
