88877eea8f2eda6c9e715f539780b0022a800c5d
CrisUwU117 <86508220+CrisUwU117@users.noreply.github.com>
2023-07-04 22:15:05 -0600

    eliminar imagen

--- backend/controllers/images.controlles.js
+++ backend/controllers/images.controlles.js
@@ -1,10 +1,10 @@
 import {db} from '../db.js'
-import fs from 'fs';
 
+import path  from 'path'
   export const cargarimagen = (req, res) => {
     
     const { nombre_archivo, id_estudiante, id_docente } = req.body;
-  const fotoPath = req.file.path;
+  const fotoPath = path.basename(req.file.path);;
 
   const query = `INSERT INTO imagen (nombre_archivo, id_estudiante, id_docente) VALUES (?, ?, ?)`;
   const values = [fotoPath, id_estudiante, id_docente];
@@ -35,10 +35,36 @@ export const obtenerImagenes  = (req, res) => {
     } else {
       const imagenes = results.map((imagen) => ({
         id: imagen.id,
-        url: `\\${imagen.nombre_archivo}`
+        url: `http://localhost:8081/${imagen.nombre_archivo}`,
+        nombre: imagen.id,
       }));
       res.status(200).json(imagenes);
     }
   });
 };
+
+export const eliminarImagen = (req, res) => {
+  const { id, nombre_archivo } = req.body;
+
+  const query = 'DELETE FROM imagen WHERE id = ? AND nombre_archivo = ?';
+  const values = [id, nombre_archivo];
+  console.log(req.body)
+
+  db.query(query, values, (error, results) => {
+    if (error) {
+      console.error('Error al eliminar la imagen de la base de datos:', error);
+      res.status(500).json({ error: 'Error al eliminar la imagen de la base de datos' });
+    } else {
+      if (results.affectedRows === 0) {
+        console.log('La imagen no existe en la base de datos');
+        res.status(404).json({ error: 'La imagen no existe en la base de datos' });
+      } else {
+        console.log('Imagen eliminada correctamente de la base de datos');
+        res.status(200).json({ message: 'Imagen eliminada correctamente de la base de datos' });
+      }
+    }
+  });
+};
+
+
 
