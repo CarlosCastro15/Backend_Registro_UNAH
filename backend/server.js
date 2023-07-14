e64446fed030d739a071fd1ddcb5af2caa9ae58d
CrisUwU117 <86508220+CrisUwU117@users.noreply.github.com>
2023-07-13 20:00:47 -0600

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,8 @@ import jwt from'jsonwebtoken';
 import administradorRoutes from './routes/administrador.routes.js'
 import estudianteRoutes from './routes/estudiante.routes.js'
 import imagesRoutes from './routes/images.routes.js'
-import videosRoutes from './routes/videos.routes.js'
 import docenteRoutes from './routes/docente.routes.js'
-import claseRoutes from './routes/clase.routes.js'
-import edificioRoutes from './routes/edificio.routes.js'
-import seccionRoutes from './routes/seccion.routes.js'
+import solicitudRoutes from './routes/Solicitude.router.js'
 import path  from 'path'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -23,15 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(imagesRoutes) 
-app.use(videosRoutes)
 app.use(administradorRoutes)
 app.use(estudianteRoutes)
 app.use(docenteRoutes)
-app.use(claseRoutes)
-app.use(edificioRoutes)
-app.use(seccionRoutes)
+app.use(solicitudRoutes)
 app.use(express.static(path.join(__dirname, 'uploads')));
-app.use(express.static(path.join(__dirname, 'uploadss')))
 
 
 // const generarCorreo = (nombre, apellido) => {
