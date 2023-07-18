be83c6335729d35c2c4b8809ae8b1e1d192dd348
CrisUwU117 <cristalpadilla654321@gmail.com>
2023-07-17 19:00:04 -0600

    ver solicitud estudiantes

--- backend/routes/Solicitude.router.js
+++ backend/routes/Solicitude.router.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import {crearSolicitud,
-    obtenerSolicitudesPorCoordinador,ActualizarEstado} from '../controllers/Solicitud.controlles.js'
+    obtenerSolicitudesPorCoordinador,ActualizarEstado,obtenerSolicitudEs} from '../controllers/Solicitud.controlles.js'
 const router = Router()
 router.post('/Crear_Solicitud', crearSolicitud)
 router.get('/Solicitudes_Coordinador', obtenerSolicitudesPorCoordinador)
 router.put('/ActualizarEstado/:id', ActualizarEstado)
+router.get('/VerSolicitud', obtenerSolicitudEs)
 export default router 
