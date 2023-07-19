b31a993df7ddfff79bdf9404eaaf2aafee1ce37a
CarlosCastro15 <101850801+CarlosCastro15@users.noreply.github.com>
2023-07-19 17:24:47 -0600

    gggag

--- backend/routes/estudiante.routes.js
+++ backend/routes/estudiante.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {getEstudiante,getEstudianteId,deleteEstudiante,updateEstudiante,envioCorreoEstudiante,restaContraEstudiante,actualizarEstuDescri,clasesAlumno,notaEstudiante} from '../controllers/estudiante.controlles.js'
+import {getEstudiante,getEstudianteId,deleteEstudiante,updateEstudiante,envioCorreoEstudiante,restaContraEstudiante,actualizarEstuDescri,clasesAlumno,notaEstudiante,clasesByIdEstudiante,verificarRequisito,verificarHorario,matriculaSeccion} from '../controllers/estudiante.controlles.js'
 
 const router = Router()
 
@@ -22,5 +22,18 @@ router.get('/claseAlumno/:id_clase', clasesAlumno)
 
 //editar nota del estudiante segun num_cuenta en la tabla clases_pasadas Roberto
 router.put('/clase-pasada-nota/:id_clase/:id_estudiante', notaEstudiante)
+
+//-------------
+//OBTENER CLASESFALTANTES POR MEDIO DEL ID DEL ESTUDIANTE
+router.get('/clasesFaltantesEstudiante/:num_cuenta', clasesByIdEstudiante)
+
+
+router.get('/verificar-requisitos/:idClase', verificarRequisito)
+
+
+router.get('/verificar-horario/:idSeccion/:num_cuenta/:anio/:periodo', verificarHorario)
+
+
+router.post('/insertMatricula', matriculaSeccion);
 
 export default router 
