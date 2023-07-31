e3f6350130eea295711f7c4e87e0e827932111d2
CrisUwU117 <cristalpadilla654321@gmail.com>
2023-07-30 20:07:55 -0600

    .

--- backend/controllers/Solicitud.controlles.js
+++ backend/controllers/Solicitud.controlles.js
@@ -1,8 +1,9 @@
 import { db } from '../db.js';
-
+import path  from 'path'
 export const crearSolicitud = (req, res) => {
   const { tipo_solicitud, num_cuenta, justificacion, id_carrera, id_centro, id_clase } = req.body;
-  const estado = "pendiente";
+  const nombre_archivo = path.basename(req.file.path);
+  const estado = "Pendiente";
   const consultaDocente = `
     SELECT d.num_empleado
     FROM docente d
@@ -14,8 +15,8 @@ export const crearSolicitud = (req, res) => {
       AND d.carrera_id = e.carrera_id
   `;
   const query = `INSERT INTO solicitud (tipo_solicitud, num_cuenta, num_empleado, justificacion,
-     estado, id_carrera, id_centro, id_clase)
-  VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+     estado, id_carrera, id_centro, id_clase,documento)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
   db.query(consultaDocente, [num_cuenta], (error, results) => {
     if (error) {
@@ -23,8 +24,13 @@ export const crearSolicitud = (req, res) => {
       res.status(500).json({ error: 'Error al guardar la solicitud' });
     } else {
       const num_empleado = results[0]?.num_empleado;
+      
+      
+      const idCarrera =!isNaN (id_carrera) ? parseInt(id_carrera) : null;
+      const idCentro = !isNaN(id_centro) ? parseInt(id_centro) : null;
+      const idClase = !isNaN(id_clase) ? parseInt(id_clase) : null;
 
-      const values = [tipo_solicitud, num_cuenta, num_empleado, justificacion, estado, id_carrera, id_centro, id_clase];
+      const values = [tipo_solicitud, num_cuenta, num_empleado, justificacion, estado, idCarrera, idCentro, idClase, nombre_archivo];
       db.query(query, values, (error, results) => {
         if (error) {
           console.error('Error al guardar la solicitud:', error);
@@ -37,6 +43,11 @@ export const crearSolicitud = (req, res) => {
     }
   });
 };
+
+
+
+
+
 
 
 
@@ -56,7 +67,7 @@ export const obtenerSolicitudesPorCoordinador = (req, res) => {
     LEFT JOIN
       centro ON solicitud.id_centro = centro.id
     WHERE
-      solicitud.num_empleado = ? AND solicitud.estado = 'pendiente'
+      solicitud.num_empleado = ? 
   `;
 
   db.query(query, values, (error, results) => {
@@ -161,13 +172,15 @@ export const obtenerSolicitudesPorCoordinador = (req, res) => {
   export const ActualizarEstado = (req, res) => {
   const { id } = req.params;
   const { estado } = req.body;
+  const { observacion } = req.body;
 
   // Consultar la solicitud para obtener los datos necesarios
   const selectQuery = `
-    SELECT tipo_solicitud, num_cuenta, id_carrera, id_centro, id_clase
-    FROM solicitud
-    WHERE id = ?
-  `;
+  SELECT s.tipo_solicitud, s.num_cuenta, s.id_carrera, s.id_centro, s.id_clase, e.correo_institucional
+  FROM solicitud s
+  JOIN estudiante e ON s.num_cuenta = e.num_cuenta
+  WHERE s.id = ?;
+`;
   const selectValues = [id];
 
   db.query(selectQuery, selectValues, (err, result) => {
@@ -182,11 +195,11 @@ export const obtenerSolicitudesPorCoordinador = (req, res) => {
 
         // Actualizar el estado en la tabla de solicitud
         const updateEstadoQuery = `
-          UPDATE solicitud
-          SET estado = ?
-          WHERE id = ?
+        UPDATE solicitud
+        SET estado = ?, observacion = ?
+        WHERE id = ?
         `;
-        const updateEstadoValues = [estado, id];
+        const updateEstadoValues = [estado, observacion,id];
 
         db.query(updateEstadoQuery, updateEstadoValues, (err, result) => {
           if (err) {
--- backend/routes/Solicitude.router.js
+++ backend/routes/Solicitude.router.js
@@ -1,9 +1,25 @@
 import {Router} from 'express'
 import {crearSolicitud,
-    obtenerSolicitudesPorCoordinador,ActualizarEstado,obtenerSolicitudEs,Centro,PagoReposicionEstudiante } from '../controllers/Solicitud.controlles.js'
-
+    obtenerSolicitudesPorCoordinador,
+    ActualizarEstado,
+    obtenerSolicitudEs,
+    Centro,
+    PagoReposicionEstudiante }
+     from '../controllers/Solicitud.controlles.js'
+     import multer from 'multer';
+ 
+     const storage = multer.diskStorage({
+         destination: function (req, file, cb) {
+           cb(null, 'uploads/'); // Carpeta donde se guardarán las fotos
+         },
+         filename: function (req, file, cb) {
+           cb(null, file.originalname);
+         }
+       });
+       const upload = multer({ storage: storage });
+     
 const router = Router()
-router.post('/Crear_Solicitud', crearSolicitud)
+router.post('/Crear_Solicitud',upload.single('Documento'), crearSolicitud)
 router.get('/Solicitudes_Coordinador', obtenerSolicitudesPorCoordinador)
 router.put('/ActualizarEstado/:id', ActualizarEstado)
 router.get('/VerSolicitud', obtenerSolicitudEs)
