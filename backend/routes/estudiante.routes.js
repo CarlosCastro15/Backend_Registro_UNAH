076728005a7a83369c3429a0b1a37d0b12a01f38
CrisUwU117 <cristalpadilla654321@gmail.com>
2023-08-04 07:58:27 -0600

    Historial pdf

--- backend/routes/estudiante.routes.js
+++ backend/routes/estudiante.routes.js
@@ -1,5 +1,16 @@
 import {Router} from 'express'
-import {getEstudiante,getEstudianteId,deleteEstudiante,updateEstudiante,envioCorreoEstudiante,restaContraEstudiante,actualizarEstuDescri,notaEstudiante,clasesByIdEstudiante,verificarRequisito,verificarHorario,matriculaSeccion,eliminarClase, clases_matriculadas,clasesAlumno,insertarclasepasada,estudianteSeccionObtener} from '../controllers/estudiante.controlles.js'
+import {getEstudiante,
+    getEstudianteId,
+    deleteEstudiante,
+    updateEstudiante,
+    envioCorreoEstudiante
+    ,restaContraEstudiante,
+    actualizarEstuDescri,
+    notaEstudiante,clasesByIdEstudiante
+    ,verificarRequisito,verificarHorario,
+    matriculaSeccion,eliminarClase, clases_matriculadas
+    ,clasesAlumno,insertarclasepasada,estudianteSeccionObtener,
+    clases_historial} from '../controllers/estudiante.controlles.js'
 
 const router = Router()
 
@@ -34,18 +45,15 @@ router.put('/clase-pasada-nota/:id_clase/:id_estudiante', notaEstudiante)
 //OBTENER CLASESFALTANTES POR MEDIO DEL ID DEL ESTUDIANTE
 router.get('/clasesFaltantesEstudiante/:num_cuenta', clasesByIdEstudiante)
 
-
 router.get('/verificar-requisitos/:idClase', verificarRequisito)
 
-
 router.get('/verificar-horario/:idSeccion/:num_cuenta/:anio/:periodo', verificarHorario)
-
 
 router.post('/insertMatricula', matriculaSeccion);
 
 router.delete('/eliminar-clase/:num_cuenta/:id_seccion', eliminarClase )
 
 router.get('/clases-matriculadas/:num_cuenta/:anio/:periodo',clases_matriculadas)
-
+router.get('/clases-historial/:num_cuenta',clases_historial)
 
 export default router 
